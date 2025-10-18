36ae1991bc60f3db54ae74789163847d97419685
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2025-10-17 22:00:48 -0300

    add_componente_texto

--- meu-projeto/src/app/page.js
+++ meu-projeto/src/app/page.js
@@ -1,13 +1,16 @@
 import Carousel from "@/components/carousel";
 import Menu from "@/components/menu";
 import Stack from "@/components/stack";
+import Teste from "@/components/teste";
 import { Ellipsis } from "lucide-react";
 export default function Home() {
   return (
     <div>
-     
       <section>
-        <div >
+        <Teste/>
+      </section>
+      <section>
+        <div>
           <Stack/>
         </div>
       </section>
---
+++ meu-projeto/src/components/teste.js
@@ -0,0 +1,8 @@
+export default function Teste(){
+    return(
+        <div className="bg-white m-4 border-2 border-black ">
+            <h1>Q</h1>
+
+        </div>
+    )
+}
