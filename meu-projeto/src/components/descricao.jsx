bf91f192f64798b467f5d4b1e6290babe4878cfc
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2025-10-31 18:35:11 -0300

    mudanca_stack

--- meu-projeto/src/components/descricao.jsx
+++ meu-projeto/src/components/descricao.jsx
@@ -1,6 +1,6 @@
 export default function Descricao(){
     return (
-        <div className="h-48 bg-[#252D6F]">
+        <div className="h-48 w-fit m-auto border rounded-2xl bg-[#252D6F]">
             <p className="text-lg text-center text-white ">
                 Desenvolvedor Front-End | Next.js | React | Tailwind CSS
             </p>
--- meu-projeto/src/components/stack.js
+++ meu-projeto/src/components/stack.js
@@ -11,16 +11,16 @@ export default function Stack() {
             <div className=" m-auto w-3/6">
                 <h1 className="text-center text-white">Habilidades</h1>
                 <div className="grid grid-cols-3 gap-1 ml-11 ">
-                    <div className="img"><FaHtml5 size={60}/></div>
-                    <div className="img"><FaCss3Alt size={60}/></div>
-                    <div className="img"><RiTailwindCssFill size={60}/> </div>
-                    <div className="img"> <FaBootstrap size={60}/></div>
-                    <div className="img"><FaWordpress size={60}/> </div>
-                    <div className="img"><FaReact size={60}/></div>
-                    <div className="img"><RiNextjsFill size={60}/></div>
-                    <div className="img"><FaJs size={60}/></div>
-                    <div className="img"><FaPython size={60}/></div>
-                    <div className="img"><FaJava size={60}/></div>
+                    <div className="img"><FaHtml5 size={60}/><p>Html</p></div>
+                    <div className="img"><FaCss3Alt size={60}/><p>Css</p></div>
+                    <div className="img"><RiTailwindCssFill size={60}/><p>Tailwind</p></div>
+                    <div className="img"> <FaBootstrap size={60}/><p>Bootstrap</p></div>
+                    <div className="img"><FaWordpress size={60}/><p>Wordpress</p></div>
+                    <div className="img"><FaReact size={60}/><p>Html</p>React</div>
+                    <div className="img"><RiNextjsFill size={60}/><p>Nextjs</p></div>
+                    <div className="img"><FaJs size={60}/><p>Javascript</p></div>
+                    <div className="img"><FaPython size={60}/><p>Python</p></div>
+                    <div className="img"><FaJava size={60}/><p>Java</p></div>
                 </div>
             </div>
             
