f6c296c08b4c4c5ea702007c91baa293c85002b8
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2025-10-30 22:27:05 -0300

    limpando_e_add_descricao

--- meu-projeto/src/app/page.js
+++ meu-projeto/src/app/page.js
@@ -1,15 +1,11 @@
-import Carousel from "@/components/carousel";
-import Menu from "@/components/menu";
+import Descricao from "@/components/descricao";
 import Stack from "@/components/stack";
-import Teste from "@/components/teste";
 import { Ellipsis } from "lucide-react";
 export default function Home() {
   return (
     <div>
       <section>
-        <p className="text-lg text-center bg-[#000319] text-white ">
-          Desenvolvedor Front-End | Next.js | React | Tailwind CSS
-        </p>
+        <Descricao/>
       </section>
       <section>
         <div>
---
+++ meu-projeto/src/components/descricao.jsx
@@ -0,0 +1,9 @@
+export default function Descricao(){
+    return (
+        <div className="h-48 bg-[#252D6F]">
+            <p className="text-lg text-center text-white ">
+                Desenvolvedor Front-End | Next.js | React | Tailwind CSS
+            </p>
+        </div>
+    )
+}
